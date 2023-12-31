ff639f874c172c156fa04dc7a5c5caa49d4c752e
tle-nopanan <P.nopanan@kkumail.com>
2024-01-01 03:05:38 +0700

    Type of data

--- main.go
+++ main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+var numberInt, numberInt2 int = 1000, 2000
+var msg string = "Hello Tle"
+
 func main() {
-	fmt.Println("Hello Tle!!!")
+	numberfloat := 25.4
+	fmt.Println(numberInt)
+	fmt.Println(numberInt2)
+	fmt.Println(numberfloat)
+	fmt.Println(msg)
+
+	fmt.Println(numberInt+numberInt2)
+	fmt.Println(float64(numberInt)+numberfloat)
+	fmt.Println(msg+" eiei")
+	fmt.Println("My money = ", numberInt2)
+
 }
