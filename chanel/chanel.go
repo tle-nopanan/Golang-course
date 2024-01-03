5fe54b7a41727fb9ac3b5580af65676a62327114
tle-nopanan <P.nopanan@kkumail.com>
2024-01-03 15:31:01 +0700

    Send data routine to routine, routine to func main === Chanel

---
+++ chanel/chanel.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func process1(c chan string, data string) {
+	c <- data
+
+}
+
+func main() {
+	ch := make(chan string, 1)
+	go process1(ch, "Data1")
+	fmt.Println(<-ch)
+	time.Sleep(5 * time.Second)
+
+}
