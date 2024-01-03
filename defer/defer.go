d9e4dfa5f6d7d0a7ccd407d6c51655447ed65e79
tle-nopanan <P.nopanan@kkumail.com>
2024-01-03 15:39:04 +0700

    defer first in last out

---
+++ defer/defer.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+func add(value1, value2 float64) {
+	result := value1 + value2
+	fmt.Println("result : ", result)
+}
+
+func loop() {
+	for i := 0; i < 10; i++ {
+		fmt.Println("i = ", i)
+	}
+}
+
+func deferloop() {
+	for j := 0; j < 10; j++ {
+		fmt.Println("j = ", j)
+	}
+}
+
+func main() {
+	loop()
+	deferloop()
+
+	// fmt.Println("Calculator !!")
+	
+	// defer last in first out
+	// 14 20 30 End
+	// defer fmt.Println("End")
+	// defer add(20, 10)
+	// defer add(15, 5)
+	// defer add(12, 2)
+
+}
